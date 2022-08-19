c78ea532a1dbe01227efda4e96da0d8180636bbb
Klabum <mklabunde40@gmail.com>
2022-08-19 19:07:50 -0300

    second commit

---
+++ README.mk
@@ -0,0 +1,1 @@
+This is the answer to challenge --Product preview card component-- from https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa/hub/product-preview-card-component-XVa-Wl1jBj 
